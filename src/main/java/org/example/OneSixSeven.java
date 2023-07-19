71deb5e87bc510fb39f5327d5b25d91273b4c69d
Ming <gaominghui464@outlook.com>
2023-07-19 14:01:05 +0100

    daily training

---
+++ src/main/java/org/example/OneSixSeven.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class OneSixSeven {
+    /*
+    Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+    find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1]
+    and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+
+    Return the indices of the two numbers, index1 and index2,
+    added by one as an integer array [index1, index2] of length 2.
+
+    The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+    Your solution must use only constant extra space.
+     */
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start <= end) {
+            if(numbers[start] + numbers[end] < target) {
+                start++;
+            } else if (numbers[start] + numbers[end] > target) {
+                end--;
+            } else {
+                result[0] = start + 1;
+                result[1] = end + 1;
+                return result;
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        twoSum(numbers, target);
+    }
+}
